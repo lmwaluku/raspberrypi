b75a112c0f7b8549b430522cf0ba7dd9b8b86599
Lloyd Mwaluku <lmwaluku@users.noreply.github.com>
2021-05-07 21:14:10 +0200

    Update 5.TempHumidityDBStore.py

--- 5.TempHumidityDBStore.py
+++ 5.TempHumidityDBStore.py
@@ -2,26 +2,31 @@ import Adafruit_DHT
 import time
 import matplotlib.pyplot as plt
 import psutil
+import datetime
 
 DHT_PIN = 4
 DHT_SENSOR = Adafruit_DHT.DHT11
 
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    e = datetime.datetime.now()
+    loc = "JHB"
+    timed = "%s:%s:%s" % (e.hour, e.minute, e.second)
+    dated = datetime.date.today()
+
     if humidity is not None and temperature is not None:
-        
-        print ("Temp {0:0.1f} C Humidy {1:0.1f} %".format(temperature, humidity))
-        
+
+        print ("Location", loc, "Time ", timed, "Date", dated, "Temp", temperature,"C Humidy", humidity,"%")
+
         import sqlite3
         conn = sqlite3.connect("temp.db")
         c = conn.cursor()
 
-        q = """INSERT INTO temph (temp, humid) VALUES (?,?)"""
-        c.execute(q, (temperature, humidity))
-        conn.commit()             
-    else: 
+        q = """INSERT INTO iotTable (loc, timed, dated, temperature, humidity) VALUES (?,?,?,?,?)"""
+        c.execute(q, (loc, timed, dated, temperature, humidity))
+        conn.commit()
+    else:
         print("Sensor Failure");
     time.sleep(3);
-    
-    #c.close()
+
     conn.close()
